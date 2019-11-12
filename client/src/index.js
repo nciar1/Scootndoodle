c5c1ebdba02cf64087b4fa328ab997b1e1e9acd1
Nicole <nciar1@ufl.edu>
2019-11-11 19:50:01 -0500

    added db

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,16 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
+
+
+
 ReactDOM.render(
     <Router>
         <App />
+            
+    
+      
+        
     </Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
